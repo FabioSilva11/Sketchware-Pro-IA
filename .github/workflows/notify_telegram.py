9a12dc57eafa0a0206587055ef50d27b6719a5a0
FabioSilva11 <produtorfabiosilva@gmail.com>
2025-08-12 03:17:59 -0400

    nova atualização

--- .github/workflows/notify_telegram.py
+++ .github/workflows/notify_telegram.py
@@ -15,14 +15,15 @@ def escape_markdown_v2(text):
     return re.sub(r'([%s])' % re.escape(escape_chars), r'\\\1', text)
 
 def main():
-    bot_token = os.environ['BOT_TOKEN']
-    chat_id = os.environ['CHAT_ID']
-    topic_id = os.environ.get('TOPIC_ID')
+    # Credenciais do Telegram - Bot Alicy
+    bot_token = "7320764076:AAFkoDkaMMlrZMsYJVoyXoiecBUrlBO7Z04"
+    chat_id = "-4851663824"
+    topic_id = None  # Deixe None se não usar tópicos
     
     commit_author, commit_message, commit_hash, commit_hash_short = get_git_commit_info()
 
     message = (
-        f"A new [commit](https://github.com/Sketchware-Pro/Sketchware-Pro/commit/{commit_hash}) has been merged to the repository by *{commit_author}*.\n\n"
+        f"A new [commit](https://github.com/FabioSilva11/Sketchware-Pro/commit/{commit_hash}) has been merged to the repository by *{commit_author}*.\n\n"
         f"*What has changed:*\n>{commit_message}\n\n"
         f"I'm currently building it and will send you the APKs here within ~6 mins if the build is successful.\n\n#{commit_hash_short}"
     )
